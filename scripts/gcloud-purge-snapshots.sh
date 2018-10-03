e21793cfce966285de516221064d4ace9ae9f4c2
Stian Grytøyr <stian@grytoyr.net>
2018-10-03 11:45:27 +0200

    Update gcloud-purge-snapshots.sh

--- scripts/gcloud-purge-snapshots.sh
+++ scripts/gcloud-purge-snapshots.sh
@@ -12,11 +12,13 @@ echo "${SNAPSHOT_LIST_LOCAL}" | while read line ; do
   # get the snapshot name from the uri
   SNAPSHOT_NAME=${line##*/}
   
-  # get the date that the snapshot was created
-  SNAPSHOT_DATETIME="$(gcloud compute snapshots describe ${SNAPSHOT_NAME} | grep "creationTimestamp" | cut -d " " -f 2 | tr -d \')"
+  # # get the date that the snapshot was created
+  # SNAPSHOT_DATETIME="$(gcloud compute snapshots describe ${SNAPSHOT_NAME} | grep "creationTimestamp" | cut -d " " -f 2 | tr -d \')"
 
-  # format the date
-  SNAPSHOT_DATETIME="$(date -d ${SNAPSHOT_DATETIME} +%Y%m%d)"
+  SNAPSHOT_DATETIME=$(echo "${SNAPSHOT_NAME}" | awk -F'--' '{print $2}' | awk -F'-' '{print $1$2$3}')
+  
+  # # format the date
+  # SNAPSHOT_DATETIME="$(date -d ${SNAPSHOT_DATETIME} +%Y%m%d)"
 
   echo "name: $SNAPSHOT_NAME Datetime: $SNAPSHOT_DATETIME"
 
